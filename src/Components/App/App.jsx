5a1fac64035101f2277bafc948cff8d92bfcb6a6
Daria Fadeeva <d9119046505@gmail.com>
2024-01-22 20:37:42 +0300

    added rate selection

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import data from "./data.json";
 import style from "./App.module.scss";
 
 const App = () => {
+  const [id, setId] = useState(0);
   return (
     <section>
       <h1>Тарифы</h1>
       <div className={style.container}>
         {data.map((rate) => (
-          <Card key={rate.id} rate={rate} />
+          <Card
+            key={rate.id}
+            rate={rate}
+            activeCard={rate.id === id}
+            setId={setId}
+          />
         ))}
       </div>
     </section>
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,17 +1,18 @@
 import style from "./Card.module.scss";
 
-const Card = ({ rate }) => {
-  console.log(rate)
+export default function Card({ rate, activeCard, setId }) {
+  const cl = activeCard ? `${style.active}` : null;
+
+  function handleActive() {
+    setId(rate.id);
+  }
+
   return (
-    <div className={`${style.cardItem} ${rate.selected === "active" ? style.selected : ""}`}> 
+    <div className={`${style.cardItem} ${cl}`} onClick={handleActive}>
       <div className={style.cardTitle}>{rate.title + " " + rate.value}</div>
       <div className={style.cardPrice}>{rate.value + " " + "руб/мес"}</div>
       <div className={style.cardSpeed}>{rate.speed + " " + "Мбит/сек"}</div>
       <div className={style.description}>{rate.description}</div>
     </div>
   );
-
-  
-};
-
-export default Card;
+}
